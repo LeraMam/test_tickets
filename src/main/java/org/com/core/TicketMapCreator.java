c936de93752d9f3eaec9909853a43fa2db6edf38
Valeria <valeria.mamaiko@gmail.com>
2024-06-04 22:24:17 +0300

    mistakes'

--- src/main/java/org/com/core/TicketMapCreator.java
+++ src/main/java/org/com/core/TicketMapCreator.java
@@ -8,26 +8,28 @@ public class TicketMapCreator {
     public static Map<String, Integer> createMinimumMap(ArrayList<Ticket> ticketList) {
         Map<String, Integer> minimum = new HashMap<>();
         for (Ticket ticket : ticketList) {
-            String carrierName = ticket.getCarrier();
-            String[] departureTime = ticket.getDepartureTime().split(":");
-            String[] arrivalTime = ticket.getArrivalTime().split(":");
+            if((ticket.getOriginName().equals("Владивосток")) && (ticket.getDestinationName().equals("Тель-Авив"))){
+                String carrierName = ticket.getCarrier();
+                String[] departureTime = ticket.getDepartureTime().split(":");
+                String[] arrivalTime = ticket.getArrivalTime().split(":");
 
-            int departureHour = Integer.parseInt(departureTime[0]);
-            int departureMinute = Integer.parseInt(departureTime[1]);
-            int arrivalHour = Integer.parseInt(arrivalTime[0]);
-            int arrivalMinute = Integer.parseInt(arrivalTime[1]);
+                int departureHour = Integer.parseInt(departureTime[0]);
+                int departureMinute = Integer.parseInt(departureTime[1]);
+                int arrivalHour = Integer.parseInt(arrivalTime[0]);
+                int arrivalMinute = Integer.parseInt(arrivalTime[1]);
 
-            int travelHours = arrivalHour - departureHour;
-            int travelMinutes = arrivalMinute - departureMinute;
-            int travelTime = travelHours * 60 + travelMinutes;
+                int travelHours = arrivalHour - departureHour;
+                int travelMinutes = arrivalMinute - departureMinute;
+                int travelTime = travelHours * 60 + travelMinutes;
 
-            if (minimum.containsKey(carrierName)) {
-                int currentMinTime = minimum.get(carrierName);
-                if (travelTime < currentMinTime) {
+                if (minimum.containsKey(carrierName)) {
+                    int currentMinTime = minimum.get(carrierName);
+                    if (travelTime < currentMinTime) {
+                        minimum.put(carrierName, travelTime);
+                    }
+                } else {
                     minimum.put(carrierName, travelTime);
                 }
-            } else {
-                minimum.put(carrierName, travelTime);
             }
         }
         return minimum;
--- src/main/java/org/com/core/TicketsReader.java
+++ src/main/java/org/com/core/TicketsReader.java
@@ -2,6 +2,8 @@ package org.com.core;
 
 import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -15,42 +17,54 @@ public class TicketsReader {
         int subtraction = 0;
         Map<String, Integer> minimumTimeForCarrier;
 
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        Gson gson = new GsonBuilder().setLenient().create();
+
         try (FileReader reader = new FileReader("json/tickets.json")) {
-            JsonObject jsonData = new JsonParser().parse(reader).getAsJsonObject();
-            JsonArray ticketsArray = jsonData.getAsJsonArray("tickets");
-            ArrayList<Ticket> ticketList = gson.fromJson(ticketsArray, new TypeToken<ArrayList<Ticket>>(){}.getType());
-            Collections.sort(ticketList, new ComparatorByTicketPrice());
+            JsonReader jsonReader = new JsonReader(reader);
+            jsonReader.setLenient(true);
 
-            int centerNumber = ticketList.size()/2;
-            for (Ticket ticket : ticketList) {
-                meanPrice = meanPrice + ticket.getPrice();
-            }
-            if(ticketList.size() % 2 == 0){
-                int element1 = ticketList.get(centerNumber - 1).getPrice();
-                int element2 = ticketList.get(centerNumber).getPrice();
-                medianPrice = (element1+ element2) / 2;
-            }
-            else medianPrice = ticketList.get(centerNumber).getPrice();
-            meanPrice = meanPrice / ticketList.size();
-            subtraction = meanPrice - medianPrice;
-            minimumTimeForCarrier = TicketMapCreator.createMinimumMap(ticketList);
+            JsonElement jsonElement = JsonParser.parseReader(jsonReader);
+            if (jsonElement.isJsonObject()) {
+                JsonObject jsonData = jsonElement.getAsJsonObject();
+                JsonArray ticketsArray = jsonData.getAsJsonArray("tickets");
 
-            System.out.println("Mean price for all tickets: " + meanPrice);
-            System.out.println("Median price for all tickets: " + medianPrice);
-            System.out.println("Subtraction for mean and median: " + subtraction);
-            System.out.println("\nMinimum time between VVO and TLV: ");
-            for (Map.Entry<String, Integer> entry : minimumTimeForCarrier.entrySet()) {
-                String carrier = entry.getKey();
-                int travelTime = entry.getValue();
-                System.out.println("Carrier: " + carrier
-                        + ", minimumTimeForCarrier travel time(in minutes): " + travelTime);
-            }
+                ArrayList<Ticket> ticketList = gson.fromJson(ticketsArray, new TypeToken<ArrayList<Ticket>>() {
+                }.getType());
+                Collections.sort(ticketList, new ComparatorByTicketPrice());
 
+                int centerNumber = ticketList.size()/2;
+                int count = 0;
+                for (Ticket ticket : ticketList) {
+                    if((ticket.getOriginName().equals("Владивосток")) && (ticket.getDestinationName().equals("Тель-Авив"))){
+                        count++;
+                        meanPrice = meanPrice + ticket.getPrice();
+                    }
+                }
+                if(ticketList.size() % 2 == 0){
+                    int element1 = ticketList.get(centerNumber - 1).getPrice();
+                    int element2 = ticketList.get(centerNumber).getPrice();
+                    medianPrice = (element1+ element2) / 2;
+                }
+                else medianPrice = ticketList.get(centerNumber).getPrice();
+                meanPrice = meanPrice / count;
+                subtraction = meanPrice - medianPrice;
+                minimumTimeForCarrier = TicketMapCreator.createMinimumMap(ticketList);
+
+                System.out.println("Mean price for all tickets: " + meanPrice);
+                System.out.println("Median price for all tickets: " + medianPrice);
+                System.out.println("Subtraction for mean and median: " + subtraction);
+                System.out.println("\nMinimum time between VVO and TLV: ");
+                for (Map.Entry<String, Integer> entry : minimumTimeForCarrier.entrySet()) {
+                    String carrier = entry.getKey();
+                    int travelTime = entry.getValue();
+                    System.out.println("Carrier: " + carrier
+                            + ", minimum travel time(in minutes): " + travelTime);
+                }
+            } else {
+                System.out.println("The input is not a valid JSON object.");
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
-
 }
